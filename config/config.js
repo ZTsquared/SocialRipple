e0d1b51e61f8a8145b987f26a4efc9ffb1f25318
ZTsquared <Zia.Tyebjee@Gmail.com>
2023-11-27 14:46:55 +0100

    seeders for keywords and users done

--- config/config.js
+++ config/config.js
@@ -7,6 +7,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
+    seederStorage: "sequelize", //<--allows for storage of seeding history
+    seederStorageTableName: "sequelize_data" //<---allows for storage of seeding history
   },
   test: {
     username: process.env.DB_USER,
@@ -14,6 +16,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
+    seederStorage: "sequelize", //<--allows for storage of seeding history
+    seederStorageTableName: "sequelize_data" //<---allows for storage of seeding history
   },
   production: {
     username: process.env.DB_USER,
@@ -21,5 +25,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
+    seederStorage: "sequelize", //<--allows for storage of seeding history
+    seederStorageTableName: "sequelize_data" //<---allows for storage of seeding history
   },
 };
