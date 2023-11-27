3037ec5ee49335223b27d745991baefe2fa81d08
Carol Domingues Carvalho <carolblosada@gmail.com>
2023-11-27 16:14:30 +0100

    working on users endpoints

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
-        secure: false
-}, },
-}, })
-
+        secure: false,
+      },
+    },
+  },
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
 var express = require("express");
 var router = express.Router();
+const { Op } = require("sequelize");
+const saltRounds = 10;
+var bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.send("respond with a resource");
+// });
+
+//GET
+
+router.get("/", async function (req, res, next) {
+  try {
+    const userInfo = await models.User.findAll({
+      attributes: ["username", "organisation", "latitude", "longitude"],
+      where: { id },
+    });
+    res.send(userInfo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/preferences", async function (req, res, next) {
+  try {
+    const keywords = await models.User.findOne({
+      where: {
+        id,
+      },
+      include: models.Keyword,
+    });
+    res.send(keywords);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//POST
+
+router.post("/register", async function (req, res, next) {
+  const { password } = req.body; // is this right ?
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    const userInfo = await models.User.create({
+      username,
+      password: hash,
+      organisation,
+      latitude,
+      longitude,
+    });
+    //how do i add to preference, its a junction table
+    await models.Preference.create({
+      Keywords,
+    });
+    res.send(userInfo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
