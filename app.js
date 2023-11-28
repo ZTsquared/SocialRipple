b6913c0038f52b3750cc7df2706d1610f31904f9
ClaireHowley <clairehowley2@gmail.com>
2023-11-28 13:21:04 +0100

    actions endpoints WIP

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const cors = require("cors");
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const actionsRouter = require("./routes/actions");
-const createActionRouter = require("./routes/actions");
-
 var app = express();
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,10 +18,7 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/actions", actionsRouter);
-app.use("/createaction", createActionRouter);
-
-app.use(cors());
+app.use("/api/users", usersRouter);
+app.use("/api/actions", actionsRouter);
 
 module.exports = app;
--- routes/actions.js
+++ routes/actions.js
@@ -1,27 +1,60 @@
 const express = require("express");
 const router = express.Router();
-const { Model } = require("sequelize");
-const { Op } = require("sequelize");
 const models = require("../models");
+const { Op } = require("sequelize");
 
-// get all actions that have a specific keyword
-app.get("/actions", async (req, res) => {
+// create an action
+router.post("/", async (req, res) => {
+	try {
+		const {
+			online,
+			in_person,
+			start_time,
+			end_time,
+			is_group,
+			name,
+			description,
+			online_link,
+			latitude,
+			longitude,
+		} = req.body;
+
+		const newAction = await models.Action.create({
+			online,
+			in_person,
+			start_time,
+			end_time,
+			is_group,
+			name,
+			description,
+			online_link,
+			latitude,
+			longitude,
+		});
+		res.status(201).send({ message: "Action created successfully" });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
+
+// get all actions by a specific keyword_id
+router.get("/", async (req, res) => {
 	try {
 		const keyword_id = req.query.keyword_id; // get keyword from req
 
-		const actions = await models.Actions.findAll({
+		const actions = await models.Action.findAll({
 			where: {
 				Keyword: keyword_id,
 			},
-			// include: [
-			// 	{
-			// 		model: models.Keyword,
-
-			// 		where: {
-			// 			keyword_id,
-			// 		},
-			// 	},
-			// ],
+			include: [
+				{
+					model: models.Keyword,
+					where: {
+						id: keyword_id,
+					},
+				},
+			],
 		});
 
 		res.send(actions);
@@ -30,3 +63,27 @@ app.get("/actions", async (req, res) => {
 		res.status(500).send(error);
 	}
 });
+
+// get all info of action by action_id
+router.get("/:action_id", async (req, res) => {
+	const action_id = req.params.action_id;
+	try {
+		const action = await models.Action.findAll({
+			where: {
+				id: action_id,
+			},
+			// include: {
+			// 	model: models.Keyword,
+			// 	where: {
+			// 		id: keyword_id,
+			// 	},
+			// },
+		});
+		res.send(action);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send(error);
+	}
+});
+
+module.exports = router;
