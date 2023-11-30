f44b099bb4a20b71b9cefda4195ba1dafa49cbdb
Carol Domingues Carvalho <carolblosada@gmail.com>
2023-11-30 11:31:46 +0100

    changes to actionsMenu page

--- client/src/pages/ActionsMenu.jsx
+++ client/src/pages/ActionsMenu.jsx
@@ -1,252 +1,198 @@
 import React, { useEffect, useRef, useState, useCallback } from "react";
 import {
-	GoogleMap,
-	useLoadScript,
-	useJsApiLoader,
-	Marker,
+  GoogleMap,
+  useLoadScript,
+  useJsApiLoader,
+  Marker,
 } from "@react-google-maps/api";
 import useAuth from "../hooks/useAuth";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export default function ActionsMenu() {
-	const { isLoggedIn, onLogout, onLogin } = useAuth();
-	const { individualAction, setIndividualAction } = useState([]);
-	const { groupAction, setGroupAction } = useState([]);
+  const { isLoggedIn, onLogout, onLogin } = useAuth();
+  const [actions, setActions] = useState([]);
 
-	const dummyActionsArray = [
-		{
-			id: 1,
-			online: false,
-			in_person: true,
-			start_time: "11:00 03-12-2023",
-			end_time: "11:00 04-12-2023",
-			is_group: true,
-			name: "a dummy action",
-			description: "description of the first action",
-			organiser_id: 3,
-			online_link: "www.alink.org",
-			latitude: "555",
-			longitude: "666",
-		},
-		{
-			id: 2,
-			online: false,
-			in_person: true,
-			start_time: "11:00 23-11-2023",
-			end_time: "11:00 30-11-2023",
-			is_group: true,
-			name: "another dummy action",
-			description: "description of the second action",
-			organiser_id: 5,
-			online_link: "www.anotherlink.org",
-			latitude: "777",
-			longitude: "999",
-		},
-	];
+  useEffect(() => {
+    getLocation();
+    getActionsOfTheWeek();
+  }, []);
 
-	const dummyUserLocationsArray = [
-		{
-			latitude: 41.571417,
-			longitude: 2.01803,
-			street: "Ample",
-			number: 50,
-			city: "Terrassa",
-		},
-		{
-			latitude: 41.572108,
-			longitude: 2.017086,
-			street: "Ample",
-			number: 128,
-			city: "Terrassa",
-		},
-		{
-			latitude: 41.569009,
-			longitude: 2.017235,
-			street: "Ample",
-			number: 91,
-			city: "Terrassa",
-		},
-	];
-	// useEffect(() => {
-	//   getActionsOfTheWeek();
-	// }, []);
+  const containerStyle = {
+    width: "400px",
+    height: "400px",
+  };
 
-	const dummyRequirementsArray = [
-		{
-			id: 1,
-			action_id: dummyActionsArray[0].id,
-			description: "wash the wosh",
-		},
-	];
+  const center = {
+    lat: 41.571417,
+    lng: 2.01803,
+  };
 
-	const containerStyle = {
-		width: "400px",
-		height: "400px",
-	};
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
 
-	const center = {
-		lat: 41.571417,
-		lng: 2.01803,
-	};
+  //state:
+  const [locationMarker, setLocationMarker] = useState();
+  const [actionMarkers, setActionMarkers] = useState();
+  const [map, setMap] = useState(null);
 
-	const { isLoaded } = useJsApiLoader({
-		id: "google-map-script",
-		googleMapsApiKey: "YOUR_API_KEY",
-	});
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
 
-	//state:
-	const [locationMarker, setLocationMarker] = useState();
-	const [actionMarkers, setActionMarkers] = useState();
-	const [map, setMap] = useState(null);
+    setMap(map);
+  }, []);
 
-	const onLoad = useCallback(function callback(map) {
-		// This is just an example of getting and using the map instance!!! don't just blindly copy!
-		const bounds = new window.google.maps.LatLngBounds(center);
-		map.fitBounds(bounds);
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+  function handleLogout() {
+    console.log("Logged out");
+    onLogout();
+    navigate("/");
+  }
 
-		setMap(map);
-	}, []);
+  const getLocation = async (street, number, city) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=carrer%20${street}%20${number}%20${city}&key=AIzaSyCGHIA__546ykAp5aVLx19mpq0fP_OeZhs`
+      );
+      const responseToJson = await response.json();
 
-	const onUnmount = useCallback(function callback(map) {
-		setMap(null);
-	}, []);
-	function handleLogout() {
-		console.log("Logged out");
-		// onLogout();
-		navigate("/");
-	}
+      setLocationMarker(responseToJson);
+      // console.log(responseToJson.results[0].geometry.location)
+      // return responseToJson.results[0].geometry.location
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-	useEffect(() => {
-		getLocation();
-	}, []);
+  function markerClick(markerIndex) {
+    console.log(`marker ${markerIndex} clicked`);
+  }
 
-	const getLocation = async (street, number, city) => {
-		try {
-			const response = await fetch(
-				`https://maps.googleapis.com/maps/api/geocode/json?address=carrer%20${street}%20${number}%20${city}&key=AIzaSyCGHIA__546ykAp5aVLx19mpq0fP_OeZhs`
-			);
-			const responseToJson = await response.json();
+  async function getActionsOfTheWeek() {
+    try {
+      const response = await fetch(`api/actions`);
+      const data = await response.json();
+      setActions(data);
+      console.log(actions);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-			setLocationMarker(responseToJson);
-			// console.log(responseToJson.results[0].geometry.location)
-			// return responseToJson.results[0].geometry.location
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+  // I need to show all the actions markers on the map.
+  // the actions come with an address, so we need to grab them with the getLocation (maybe?) for each one.
+  // how do I fetch multiple actions in REACT omg
+  // should I do it when we MAP to instantiate the markers?
 
-	function markerClick(markerIndex) {
-		console.log(`marker ${markerIndex} clicked`);
-	}
+  return (
+    // maybe show the actions in a 2 columns format: |group| |individual|
+    <div>
+      <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav>
+          Nav bar of our awesome app{" "}
+          {isLoggedIn ? (
+            <div>
+              <Link to="/Action/Create" className="btn btn-success">
+                Create Action
+              </Link>
+              <Link to="/Profile" className="btn btn-success">
+                Profile
+              </Link>
+              <button className="btn btn-success" onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div>
+              <Link to="/Login" className="btn btn-success">
+                Login
+              </Link>
+              <Link to="/Register" className="btn btn-success">
+                Sign In
+              </Link>
+            </div>
+          )}
+        </nav>
+      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <h3>Group Actions</h3>
+            {actions.map(
+              (action, index) =>
+                action.is_group && (
+                  <div>
+                    <div key={index} className="card">
+                      <div>
+                        <b>{action.name}</b>
+                      </div>
+                      <div>{action.description}</div>
+                      <div>Start time: {action.start_time}</div>
+                      <div>End time: {action.end_time}</div>
+                      <div>
+                        place: lat:{action.latitude}long:{action.longitude}{" "}
+                        we'll see how we display this
+                      </div>
+                      <div>we need to display the requirements here</div>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
 
-	//WAITING FOR ACTIONS ENDPOINTS TO BE FINISHED TO DO THIS PART - carol
-	// async function getActionsOfTheWeek() {
-	//   try {
-	//     const response = await fetch(`/actions/`);
-	//     const data = await response.json();
-	//need some kind of if statement to check if its a group or a individual action
-	//     setIndividualAction(dataOfIndividualAction);
-	//     setGroupAction(dataOfGroupAction);
-	//   } catch (error) {
-	//     console.log(error);
-	//   }
-	// }
+          <div className="col-sm">
+            <h3>Individual Actions</h3>
+            {actions.map(
+              (action, index) =>
+                !action.is_group && (
+                  <div>
+                    <div key={index} className="card">
+                      <div>
+                        <b>{action.name}</b>
+                      </div>
+                      <div>{action.description}</div>
+                      <div>Start time: {action.start_time}</div>
+                      <div>End time: {action.end_time}</div>
+                      <div>
+                        place: lat:{action.latitude}long:{action.longitude}{" "}
+                        we'll see how we display this
+                      </div>
+                      <div>we need to display the requirements here</div>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
 
-	// I need to show all the actions markers on the map.
-	// the actions come with an address, so we need to grab them with the getLocation (maybe?) for each one.
-	// how do I fetch multiple actions in REACT omg
-	// should I do it when we MAP to instantiate the markers?
-
-	return (
-		// maybe show the actions in a 2 columns format: |group| |individual|
-		<div>
-			<header className="navbar navbar-expand-lg navbar-light bg-light">
-				<nav>
-					Nav bar of our awesome app{" "}
-					{isLoggedIn ? (
-						<div>
-							<Link to="/Action/Create" className="btn btn-success">
-								Create Action
-							</Link>
-							<Link to="/Profile" className="btn btn-success">
-								Profile
-							</Link>
-							<button className="btn btn-success" onClick={handleLogout}>
-								Logout
-							</button>
-						</div>
-					) : (
-						<div>
-							<Link to="/Login" className="btn btn-success">
-								Login
-							</Link>
-							<Link to="/Register" className="btn btn-success">
-								Sign In
-							</Link>
-						</div>
-					)}
-				</nav>
-			</header>
-			<div className="container">
-				<div className="row">
-					<div className="col-sm">
-						<h3>Group Actions</h3>
-						{dummyActionsArray.map((action, index) => (
-							<div key={index} className="card">
-								<div>
-									<b>{action.name}</b>
-								</div>
-								<div>{action.description}</div>
-								<div>Start time: {action.start_time}</div>
-								<div>End time: {action.end_time}</div>
-								<div>
-									place: lat:{action.latitude}long:{action.longitude} we'll see
-									how we display this
-								</div>
-								<div>we need to display the requirements here</div>
-							</div>
-						))}
-					</div>
-
-					<div className="col-sm">
-						<h3>Individual Actions</h3>
-					</div>
-
-					<div className="col-sm">
-						<div>
-							<h3>a decent looking map</h3>
-							{isLoaded && (
-								<GoogleMap
-									mapContainerStyle={containerStyle}
-									center={center}
-									zoom={10}
-									onLoad={onLoad}
-									onUnmount={onUnmount}>
-									{dummyUserLocationsArray.map((location, i) => (
-										<Marker
-											onClick={() => markerClick(i)}
-											key={i}
-											position={{
-												lat: location.latitude,
-												lng: location.longitude,
-											}}
-										/>
-									))}
-									{/* // dummyUserLocationsArray.map((location, i) => <Marker onClick={markerClick} key={i} position={() => getLocation(location.street, location.number, location.city )}/>)} */}
-								</GoogleMap>
-							)}
-						</div>
-					</div>
-				</div>
-				<footer className="footer">
-					<nav className="navbar navbar-expand-lg navbar-light bg-light">
-						<Link to="/Home" className="btn btn-success">
-							Homepage
-						</Link>
-					</nav>
-				</footer>
-			</div>
-		</div>
-	);
+          <div className="col-sm">
+            <div>
+              <h3>a decent looking map</h3>
+              {isLoaded && (
+                <GoogleMap
+                  mapContainerStyle={containerStyle}
+                  center={center}
+                  zoom={10}
+                  onLoad={onLoad}
+                  onUnmount={onUnmount}
+                ></GoogleMap>
+              )}
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/Home" className="btn btn-success">
+              Homepage
+            </Link>
+          </nav>
+        </footer>
+      </div>
+    </div>
+  );
 }
