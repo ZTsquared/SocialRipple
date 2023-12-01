74d6e5cb612e3b83fbf63a6488c959b6ff97fb1f
ClaireHowley <clairehowley2@gmail.com>
2023-12-01 13:04:20 +0100

    css single action page WIP

--- client/src/pages/Action.jsx
+++ client/src/pages/Action.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./Action.css";
+import "../Action.css";
 import { useCountdown } from "../hooks/useCountdown";
+import { Tabs, Tab } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Action() {
 	const [oneAction, setOneAction] = useState([]);
@@ -31,91 +33,108 @@ export default function Action() {
 
 	return (
 		<div>
-			<div className="container">
-				<div className="left-side">
-					<h1> {oneAction.name} </h1>
-					<h4> Description </h4>
-					<p>{oneAction.description}</p>
+			{/* <div className="left-side"> */}
+			<div className="actioncontainer-css">
+				<h1> {oneAction.name} </h1>
+				<div className="tabs">
+					<Tabs defaultActiveKey="description" id="tabs">
+						<Tab
+							eventKey="description"
+							title="Description"
+							className="actionTabContent-css ">
+							<p>{oneAction.description}</p>
+							{oneAction.Keywords && (
+								<div>
+									<ul>
+										{oneAction.Keywords.map((keyword) => (
+											<li className="badge bg-primary" key={keyword.id}>
+												{keyword.keyword}
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+						</Tab>
 
-					<div>
-						{oneAction.online
-							? `Follow this link: ${oneAction.online_link} `
-							: `Location: ${oneAction.longitude} ${oneAction.latitude}`}
-
-						{/* // {oneAction.online && `Follow this link: ${oneAction.online_link} `} :{" "}
+						<Tab
+							eventKey="location"
+							title="Location"
+							className="actionTabContent-css ">
+							{oneAction.online
+								? `Follow this link: ${oneAction.online_link} `
+								: `Location: ${oneAction.longitude} ${oneAction.latitude}`}{" "}
+							{/* // {oneAction.online && `Follow this link: ${oneAction.online_link} `} :{" "}
 				// {oneAction.in_person && `${oneAction.longitude} ${oneAction.latitude}`} */}
-					</div>
+						</Tab>
 
-					<div>
-						{oneAction.start_time
-							? `Start time: ${oneAction.start_time}`
-							: "Take as long as you want!"}{" "}
-					</div>
+						<Tab
+							eventKey="when"
+							title="When?"
+							className="actionTabContent-css ">
+							{oneAction.start_time
+								? `Start time: ${oneAction.start_time}`
+								: "Take as long as you want!"}{" "}
+							{oneAction.end_time ? `End time: ${oneAction.end_time}` : ""}{" "}
+						</Tab>
 
-					<div>
-						{" "}
-						{oneAction.end_time ? `End time: ${oneAction.end_time}` : ""}{" "}
-					</div>
-				</div>
-				<div className="right-side">
-					<div>
-						{oneAction.Keywords && oneAction.Keywords.length > 0 && (
-							<div>
-								Keywords:
-								<ul>
-									{oneAction.Keywords.map((keyword) => (
-										<li key={keyword.id}>{keyword.keyword}</li>
-									))}
-								</ul>
-							</div>
-						)}
-					</div>
-
-					<div>
-						{oneAction.Requirements && oneAction.Requirements.length > 0 && (
-							<div>
-								Requirements
-								{oneAction.Requirements.map((requirement) => (
-									<div key={requirement.id}>
-										<div>Description: {requirement.description}</div>
-										<div>Capacity: {requirement.capacity}</div>
-
-										{requirement.Volunteerships &&
-											requirement.Volunteerships.length > 0 && (
-												<div>
-													Other Volunteers
-													{requirement.Volunteerships.map((volunteership) => (
-														<div key={volunteership.id}>
-															<div>{volunteership.User.username}</div>
-															{/* <div>Completed: {volunteership.completed}</div> */}
-														</div>
-													))}
+						<Tab
+							eventKey="requirements"
+							title="Requirements"
+							className="actionTabContent-css">
+							{oneAction.Requirements && oneAction.Requirements.length > 0 && (
+								<div className="container">
+									{oneAction.Requirements.map((requirement) => (
+										<div key={requirement.id}>
+											<div className="row">
+												<div className="col">{requirement.description}</div>
+												<div className="col">
+													{requirement.capacity} people needed
+													<div id="actionVolunteerContent-css">
+														{requirement.Volunteerships &&
+															requirement.Volunteerships.length > 0 && (
+																<div>
+																	Volunteers:{" "}
+																	{requirement.Volunteerships.map(
+																		(volunteership) => (
+																			<div key={volunteership.id}>
+																				<div>{volunteership.User.username}</div>
+																				{/* <div>Completed: {volunteership.completed}</div> */}
+																			</div>
+																		)
+																	)}
+																</div>
+															)}
+													</div>
 												</div>
-											)}
-									</div>
-								))}
+											</div>
+										</div>
+									))}
+								</div>
+							)}
+							<button>Sign up!</button>
+						</Tab>
+					</Tabs>
+				</div>
+
+				<div className="actionBottomContainer-css">
+					<div className="countdown">
+						{days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
+							<div>Countdown is over!</div>
+						) : (
+							<div>
+								Remaining time: {days} days, {hours} hours, {minutes} minutes,{" "}
+								{seconds} seconds
 							</div>
 						)}
 					</div>
-				</div>
-			</div>
 
-			<div>
-				{days === 0 && hours === 0 && minutes === 0 && seconds === 0 ? (
-					<div>Countdown is over!</div>
-				) : (
-					<div>
-						Remaining time: {days} days, {hours} hours, {minutes} minutes,{" "}
-						{seconds} seconds
+					{/* back to actionsmenu page */}
+					<div className="buttonSection" id="singleButton">
+						<button className="backButton" onClick={() => navigate(-1)}>
+							Back
+						</button>
 					</div>
-				)}
-			</div>
-
-			{/* back to actionsmenu page */}
-			<div className="buttonSection" id="singleButton">
-				<button className="backButton" onClick={() => navigate(-1)}>
-					Back
-				</button>
+				</div>
 			</div>
 		</div>
 	);
