e1796d57b3f5c59489d94db600161007179a0e09
ClaireHowley <clairehowley2@gmail.com>
2023-12-04 14:35:19 +0100

    action page layout changed, joinAction route WIP

--- routes/auth.js
+++ routes/auth.js
@@ -9,49 +9,49 @@ const supersecret = process.env.SUPER_SECRET;
 require("dotenv").config();
 
 router.post("/register", async function (req, res, next) {
-  console.log(req.body);
-  const { username, password, organisation, latitude, longitude } =
-    req.body.user;
-  const { preferences } = req.body;
-  try {
-    console.log("Request Body:", req.body);
-    console.log("Keywords:", preferences);
-    const newUserInfo = await models.User.create({
-      username,
-      password,
-      organisation,
-      longitude,
-      latitude,
-    });
-    await newUserInfo.addKeywords(preferences);
-    res.send("Register succesful");
-  } catch (error) {
-    res.status(500).send(error);
-  }
+	console.log(req.body);
+	const { username, password, organisation, latitude, longitude } =
+		req.body.user;
+	const { preferences } = req.body;
+	try {
+		console.log("Request Body:", req.body);
+		console.log("Keywords:", preferences);
+		const newUserInfo = await models.User.create({
+			username,
+			password,
+			organisation,
+			longitude,
+			latitude,
+		});
+		await newUserInfo.addKeywords(preferences);
+		res.send("Register succesful");
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 router.post("/login", async function (req, res, next) {
-  const { username, password } = req.body;
-  // console.log(req.body);
-  try {
-    // console.log(username);
-    const user = await models.User.findOne({
-      where: { username },
-    });
+	const { username, password } = req.body;
+	// console.log(req.body);
+	try {
+		// console.log(username);
+		const user = await models.User.findOne({
+			where: { username },
+		});
 
-    if (user) {
-      // console.log(user);
-      const correctPassword = await bcrypt.compare(password, user.password);
-      if (!correctPassword) throw new Error("Incorrect password");
-      var token = jwt.sign({ user_id: user.id }, supersecret);
-      res.send({
-        message: "Login successful, here is your token",
-        token,
-      });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
+		if (user) {
+			// console.log(user);
+			const correctPassword = await bcrypt.compare(password, user.password);
+			if (!correctPassword) throw new Error("Incorrect password");
+			var token = jwt.sign({ user_id: user.id }, supersecret);
+			res.send({
+				message: "Login successful, here is your token",
+				token,
+			});
+		}
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 module.exports = router;
