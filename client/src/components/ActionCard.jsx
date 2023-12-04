8c10e4ed904d8924fc341bda2c611a400fab2c22
Carol Domingues Carvalho <carolblosada@gmail.com>
2023-12-04 12:25:47 +0100

    lat and lng work at registration

--- client/src/components/ActionCard.jsx
+++ client/src/components/ActionCard.jsx
@@ -2,85 +2,113 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useCountdown } from "../hooks/useCountdown";
-import TextTruncate from 'react-text-truncate';
+import TextTruncate from "react-text-truncate";
 
-export default function ActionCard({action}) {
+export default function ActionCard({ action }) {
+  const [eventDate, setEventDate] = useState(
+    new Date(action.start_time).toLocaleDateString("en-UK", {
+      weekday: "long",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    })
+  );
+  const [eventStartTime, setEventStartTime] = useState(
+    new Date(action.start_time).toLocaleTimeString("en-UK", {
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+  );
+  const [eventEndTime, setEventEndTime] = useState(
+    new Date(action.end_time).toLocaleTimeString("en-UK", {
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+  );
 
-    const [eventDate, setEventDate] = useState(new Date(action.start_time).toLocaleDateString("en-UK", {
-        weekday: 'long',
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-    }))
-    const [eventStartTime, setEventStartTime] = useState(new Date(action.start_time).toLocaleTimeString("en-UK", {hour: '2-digit', minute:'2-digit'}))
-    const [eventEndTime, setEventEndTime] = useState(new Date(action.end_time).toLocaleTimeString("en-UK", {hour: '2-digit', minute:'2-digit'}))
-    
-    const targetDate = new Date(action.start_time).getTime();
-	const { days, hours, minutes, seconds } = useCountdown(targetDate);
-
-
-    
+  const targetDate = new Date(action.start_time).getTime();
+  const { days, hours, minutes, seconds } = useCountdown(targetDate);
 
   return (
-    <div 
-        style={{
-          width: "18rem",
-          height: "28rem",
-          backgroundColor: "#c4c1e0",
-          color: "white",
-          padding: "10px",
-          marginBottom: "50px",
-          borderRadius: "6px",
-        }}
+    <div
+      style={{
+        width: "18rem",
+        height: "28rem",
+        backgroundColor: "#c4c1e0",
+        color: "white",
+        padding: "10px",
+        marginBottom: "50px",
+        borderRadius: "6px",
+      }}
     >
-        <Link to={`/Action/View/${action.id}>`}>
-            <img
-            src="https://www.teameacc.org/wp-content/uploads/sites/8/2022/04/events.jpg"
-            className="card-img-top"
-            alt="..."
-            style={{ marginTop: "10px",  borderRadius: "4px" }}
-            />
-            <br /><hr />
-            <div className="card-body">
-                <div style={{ height: "5rem" }}>
-                    <div className="card-title fs-5" style={{ height: "0.5rem" }}>{action.name}</div>
-                    <br />
-                    <TextTruncate
-                        className="card-text fw-light"
-                        line={2}
-                        element="span"
-                        truncateText="…"
-                        text={action.description}
-                    />
-                </div>
-                <hr />
-                <div className="card-text fw-light">
-                    {action.is_group ? <div>
-                        <div className="row">
-                            <div className="col-3">{`When: `} </div>
-                            <div className="col">{eventDate} </div>
-                        </div> 
-                        <div className="row">
-                        <div className="col-3"></div>
-                            <div className="col">{`${eventStartTime} - ${eventEndTime}`} </div>
-                        </div> 
-                    </div> : <div>
-                        {days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
-                            <div>Happening Now!</div>
-                        ) : (
-                            <div>
-                                {(days >=3) ? <div>Starting in: {days} days</div> : <div>Starting in: {days*24 + hours} hours, {minutes} minutes, {seconds} seconds</div>}
-                            </div>
-                        )}
-                    </div>}
-                    <div className="row">
-                        <div className="col-3">{`Where: `} </div>
-                        <div className="col">{!action.in_person && !action.online && "Anywhere!"} {action.in_person && "Barcelona"} {action.in_person && action.online && " & "} {action.online && "Online"}</div>
-                    </div> 
-                </div>
+      <Link to={`/Action/View/${action.id}>`}>
+        <img
+          src="https://www.teameacc.org/wp-content/uploads/sites/8/2022/04/events.jpg"
+          className="card-img-top"
+          alt="..."
+          style={{ marginTop: "10px", borderRadius: "4px" }}
+        />
+        <br />
+        <hr />
+        <div className="card-body">
+          <div style={{ height: "5rem" }}>
+            <div className="card-title fs-5" style={{ height: "0.5rem" }}>
+              {action.name}
             </div>
-        </Link>
+            <br />
+            <TextTruncate
+              className="card-text fw-light"
+              line={2}
+              element="span"
+              truncateText="…"
+              text={action.description}
+            />
+          </div>
+          <hr />
+          <div className="card-text fw-light">
+            {action.is_group ? (
+              <div>
+                <div className="row">
+                  <div className="col-3">{`When: `} </div>
+                  <div className="col">{eventDate} </div>
+                </div>
+                <div className="row">
+                  <div className="col-3"></div>
+                  <div className="col">
+                    {`${eventStartTime} - ${eventEndTime}`}{" "}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                {days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
+                  <div>Happening Now!</div>
+                ) : (
+                  <div>
+                    {days >= 3 ? (
+                      <div>Starting in: {days} days</div>
+                    ) : (
+                      <div>
+                        Starting in: {days * 24 + hours} hours, {minutes}{" "}
+                        minutes, {seconds} seconds
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            )}
+            <div className="row">
+              <div className="col-3">{`Where: `} </div>
+              <div className="col">
+                {!action.in_person && !action.online && "Anywhere!"}{" "}
+                {action.in_person && "Barcelona"}{" "}
+                {action.in_person && action.online && " & "}{" "}
+                {action.online && "Online"}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
-
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,53 +3,52 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import NavBar from "../components/NavBar"
-import FootBar from "../components/FootBar"
+import NavBar from "../components/NavBar";
+import FootBar from "../components/FootBar";
 
 export default function Register() {
-	const [preferences, setPreferences] = useState([]);
-	// const [userBody, setUserBody] = useState();
+  const [preferences, setPreferences] = useState([]);
+  const [userCoordinates, setUserCoordinates] = useState();
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    organisation: "",
+    street: "",
+    house_number: "",
+    city: "",
+  });
+  const [keywords, setKeywords] = useState([]);
+  let { username, password, organisation, street, house_number, city } =
+    credentials;
+  const navigate = useNavigate();
 
-	const [userCoordinates, setUserCoordinates] = useState();
-	const [credentials, setCredentials] = useState({
-		username: "",
-		password: "",
-		organisation: "",
-		// zipcode: "",
-		// latitude: userCoordinates?.lat,
-		// longitude: userCoordinates?.lng,
-		street: "",
-		house_number: "",
-		city: "",
-	});
-	const [keywords, setKeywords] = useState([]);
-	let { username, password, organisation, latitude, longitude } = credentials;
-	const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setCredentials({ ...credentials, [name]: value });
-	};
+    console.log(e.target.name);
+  };
 
-	useEffect(() => {
-		getKeywords();
-	}, []);
+  useEffect(() => {
+    getKeywords();
+  }, []);
 
-	const getKeywords = async function () {
-		try {
-			const res = await fetch(`/api/keywords/`);
-			if (!res.ok) throw new Error(`Oops!`);
+  useEffect(() => {
+    setCoordinates(street, house_number, city);
+  }, [city]);
 
-			const data = await res.json();
-			setKeywords(data);
-		} catch (error) {
-			setError(error.message);
-		}
-	};
-
+  const getKeywords = async function () {
+    try {
+      const res = await fetch(`/api/keywords/`);
+      if (!res.ok) throw new Error(`Oops!`);
+      const data = await res.json();
+      setKeywords(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   const register = async (user) => {
-    console.log(user);
     try {
       console.log("trying...");
       const { data } = await axios("/api/auth/register", {
@@ -61,172 +60,168 @@ export default function Register() {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const setCoordinates = async (street, number, city) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=carrer%20${street}%20${number}%20${city}&key=AIzaSyCGHIA__546ykAp5aVLx19mpq0fP_OeZhs`
+      );
+      const responseToJson = await response.json();
+      console.log(responseToJson);
+      setUserCoordinates(responseToJson.results[0].geometry.location);
+      return responseToJson.results[0].geometry.location;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  function handleKeywordChange(e) {
+    console.log(credentials);
+    if (e.target.checked) setPreferences((p) => [...p, e.target.value]);
+    else setPreferences((p) => p.filter((pref) => pref !== e.target.value));
   }
 
-	function handleKeywordChange(e) {
-		if (e.target.checked) setPreferences((p) => [...p, e.target.value]);
-		else setPreferences((p) => p.filter((pref) => pref !== e.target.value));
-	}
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await setCoordinates(
+      credentials.street,
+      credentials.number,
+      credentials.city
+    );
 
-	async function handleSubmit(event) {
-		event.preventDefault();
-
-		await setCoordinates(
-			credentials.street,
-			credentials.number,
-			credentials.city
-		);
-
-		register({
-			username: credentials.username,
-			password: credentials.password,
-			organisation: credentials.organisation,
-			latitude: userCoordinates?.lat,
-			longitude: userCoordinates?.lng,
-		});
-	}
-
-	const setCoordinates = async (street, number, city) => {
-		try {
-			const response = await fetch(
-				`https://maps.googleapis.com/maps/api/geocode/json?address=carrer%20${street}%20${number}%20${city}&key=AIzaSyCGHIA__546ykAp5aVLx19mpq0fP_OeZhs`
-			);
-			const responseToJson = await response.json();
-
-			console.log(responseToJson);
-			setUserCoordinates(responseToJson.results[0].geometry.location);
-			return responseToJson.results[0].geometry.location;
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
-
-
+    register({
+      username: credentials.username,
+      password: credentials.password,
+      organisation: credentials.organisation,
+      latitude: userCoordinates?.lat,
+      longitude: userCoordinates?.lng,
+    });
+  }
 
   return (
     <div>
-      <NavBar/>
-    <div className="mainMenu">
-      <div>
-        <br />
-        <div className="registerTitle-css">
-          <h2> Sign up:</h2>
-        </div>
-        <form onSubmit={() => handleSubmit(event)} action="">
-          <label htmlFor="username" className="form-label">
-            Username: <br />
-            <input
-              value={username}
-              name="username"
-              onChange={handleChange}
-              id="username"
-              type="text"
-              className="form-control"
-              />
-          </label>
+      <NavBar />
+      <div className="mainMenu">
+        <div>
           <br />
-          <br />
-          <label htmlFor="password" className="form-label">
-            Password: <br />
-            <input
-              value={password}
-              name="password"
-              onChange={handleChange}
-              id="password"
-              type="password"
-              className="form-control"
-              />
-          </label>
-          <br /> <br />
-          <label htmlFor="organisation" className="form-label">
-            Are you an organisation: <br />
-            <input
-              value={organisation}
-              name="organisation"
-              onChange={handleChange}
-              id="organisation"
-              type="organisation"
-              className="form-control"
-              />
-            <br />
-            <br />
-          </label>
-          <div>Address</div>
-          <label htmlFor="street" className="form-label">
-            <input
-              placeholder="Street Name"
-              onChange={handleChange}
-              type="text"
-              name="street"
-              id="street"
-              className="form-control"
-              />
-          </label>
-          <label htmlFor="house_number" className="form-label">
-            <input
-              placeholder="Number"
-              onChange={handleChange}
-              type="text"
-              name="house_number"
-              id="house_number"
-              className="form-control"
-            />
-          </label>{" "}
-          <label htmlFor="city" className="form-label">
-            <input
-              placeholder="City"
-              onChange={handleChange}
-              type="text"
-              name="city"
-              id="city"
-              className="form-control"
-              />
-          </label>{" "}
-          <br />
-          <br />
-          <label className="form-label">
-            What type of actions are you looking for?
-          </label>
-          <br />
-          <div className="preferencesInRegisterPage-css row justify-content-center">
-            {keywords.map((keyword, index) => (
-              <div
-              key={keyword.id}
-              className={`col-2 mb-3 d-flex justify-content-center align-items-center`}
-              style={{ margin: index % 3 === 2 ? "5px" : "10px" }}
-              >
-                <div className="d-inline-flex" style={{ gap: "67px" }}>
-                  <input
-                    id={keyword.id}
-                    value={keyword.id}
-                    type="checkbox"
-                    name="preferences"
-                    onChange={handleKeywordChange}
-                    checked={
-                      preferences.includes(keyword.id) ? "checked" : null
-                    }
-                    className="btn-check"
-                    autoComplete="off"
-                    />
-                  <label
-                    className="btn"
-                    htmlFor={keyword.id}
-                    style={{ backgroundColor: "#e4f1fe" }}
-                  >
-                    {keyword.keyword}
-                  </label>
-                </div>
-              </div>
-            ))}
+          <div className="registerTitle-css">
+            <h2> Sign up:</h2>
           </div>
+          <form onSubmit={() => handleSubmit(event)} action="">
+            <label htmlFor="username" className="form-label">
+              Username: <br />
+              <input
+                value={username}
+                name="username"
+                onChange={handleChange}
+                id="username"
+                type="text"
+                className="form-control"
+              />
+            </label>
+            <br />
+            <br />
+            <label htmlFor="password" className="form-label">
+              Password: <br />
+              <input
+                value={password}
+                name="password"
+                onChange={handleChange}
+                id="password"
+                type="password"
+                className="form-control"
+              />
+            </label>
+            <br /> <br />
+            <label htmlFor="organisation" className="form-label">
+              Are you an organisation: <br />
+              <input
+                value={organisation}
+                name="organisation"
+                onChange={handleChange}
+                id="organisation"
+                type="organisation"
+                className="form-control"
+              />
+              <br />
+              <br />
+            </label>
+            <div>Address</div>
+            <label htmlFor="street" className="form-label">
+              <input
+                placeholder="Street Name"
+                onChange={handleChange}
+                type="text"
+                name="street"
+                id="street"
+                className="form-control"
+              />
+            </label>
+            <label htmlFor="house_number" className="form-label">
+              <input
+                placeholder="Number"
+                onChange={handleChange}
+                type="text"
+                name="house_number"
+                id="house_number"
+                className="form-control"
+              />
+            </label>{" "}
+            <label htmlFor="city" className="form-label">
+              <input
+                placeholder="City"
+                onChange={handleChange}
+                type="text"
+                name="city"
+                id="city"
+                className="form-control"
+              />
+            </label>{" "}
+            <br />
+            <br />
+            <label className="form-label">
+              What type of actions are you looking for?
+            </label>
+            <br />
+            <div className="preferencesInRegisterPage-css row justify-content-center">
+              {keywords.map((keyword, index) => (
+                <div
+                  key={keyword.id}
+                  className={`col-2 mb-3 d-flex justify-content-center align-items-center`}
+                  style={{ margin: index % 3 === 2 ? "5px" : "10px" }}
+                >
+                  <div className="d-inline-flex" style={{ gap: "67px" }}>
+                    <input
+                      id={keyword.id}
+                      value={keyword.id}
+                      type="checkbox"
+                      name="preferences"
+                      onChange={handleKeywordChange}
+                      checked={
+                        preferences.includes(keyword.id) ? "checked" : null
+                      }
+                      className="btn-check"
+                      autoComplete="off"
+                    />
+                    <label
+                      className="btn"
+                      htmlFor={keyword.id}
+                      style={{ backgroundColor: "#e4f1fe" }}
+                    >
+                      {keyword.keyword}
+                    </label>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <br />
+            <button className="sigInButton-css">Sign in</button>
+          </form>
           <br />
-          <button className="sigInButton-css">Sign in</button>
-        </form>
-        <br />
+        </div>
       </div>
+      <FootBar />
     </div>
-    <FootBar/>
-  </div>
   );
-
 }
