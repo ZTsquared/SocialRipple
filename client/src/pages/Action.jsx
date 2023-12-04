b2adde595e32e3e49c0b3416110553dd0c0e31ab
ClaireHowley <clairehowley2@gmail.com>
2023-12-04 22:50:39 +0100

    action page wip

--- client/src/pages/Action.jsx
+++ client/src/pages/Action.jsx
@@ -8,181 +8,209 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/NavBar";
 
 export default function Action() {
-  const [oneAction, setOneAction] = useState([]);
-  const { ActionId } = useParams();
-  //   const [checked, setChecked] = useState(false);
-  //   const [volunteership, setVolunteership] = useState([]);
+	const [oneAction, setOneAction] = useState([]);
+	const { ActionId } = useParams();
+	//   const [checked, setChecked] = useState(false);
+	const [volunteership, setVolunteership] = useState([]);
 
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  useEffect(() => {
-    displayOneAction();
-  }, [ActionId]);
+	useEffect(() => {
+		displayOneAction();
+	}, [ActionId]);
 
-  async function displayOneAction() {
-    try {
-      const response = await fetch(`/api/actions/${ActionId}`);
-      if (!response.ok) {
-        throw new Error("Oops, something went wrong");
-      }
-      const data = await response.json();
-      setOneAction(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async function displayOneAction() {
+		try {
+			const response = await fetch(`/api/actions/${ActionId}`);
+			if (!response.ok) {
+				throw new Error("Oops, something went wrong");
+			}
+			const data = await response.json();
+			setOneAction(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  const targetDate = new Date(oneAction.end_time).getTime();
+	const targetDate = new Date(oneAction.end_time).getTime();
 
-  const { days, hours, minutes, seconds } = useCountdown(targetDate);
+	const { days, hours, minutes, seconds } = useCountdown(targetDate);
 
-  const startTime = new Date(oneAction.start_time).toLocaleDateString("en-UK", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  });
+	const startTime = new Date(oneAction.start_time).toLocaleDateString("en-UK", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+	});
 
-  const endTime = new Date(oneAction.end_time).toLocaleDateString("en-UK", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  });
+	const endTime = new Date(oneAction.end_time).toLocaleDateString("en-UK", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+	});
 
-  //trying things
+	//trying things
 
-  const volunteership = async () => {
-    try {
-      const response = await fetch(`/api/joinAction`, {
-        method: "POST",
-        data: { userId, requirementId },
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	// async function updateVolunteership(requirementId) {
+	// 	try {
+	// 		const response = await fetch(`/api/joinAction`, {
+	// 			method: "POST",
+	// 			body: JSON.stringify({ userId, requirementId }),
+	// 		});
 
-  const onClick = (e) => {
-    volunteership;
-  };
+	// 		if (response.ok) {
+	// 			const updatedData = await response.json();
+	// 			setVolunteership(updatedData);
+	// 		}
+	// 	} catch (error) {}
+	// }
 
-  return (
-    <div>
-      <NavBar />
+	// const handleCheckboxChange = (e) => {
+	// 	updateVolunteership();
+	// update for--> requirement.Volunteerships.length
+	// unclick:
+	// remove from array
+	// };
 
-      <div className="actioncontainer-css">
-        <h1> {oneAction.name} </h1>
-        <div className="tabs">
-          <Tabs defaultActiveKey="description" id="tabs">
-            <Tab
-              eventKey="description"
-              title="Description"
-              className="actionTabContent-css "
-            >
-              <p>{oneAction.description}</p>
-              {oneAction.Keywords && (
-                <div className="keywordBadges">
-                  <ul>
-                    {oneAction.Keywords.map((keyword) => (
-                      <li className="badge bg-primary" key={keyword.id}>
-                        {keyword.keyword}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </Tab>
+	// users to join events with the requirements selected
+	// const onClick = (e) => {
 
-            <Tab
-              eventKey="location"
-              title="Location"
-              className="actionTabContent-css "
-            >
-              <div>
-                <div className="container">
-                  {oneAction.online
-                    ? `Follow this link: ${oneAction.online_link} `
-                    : `Location: ${oneAction.longitude} ${oneAction.latitude}`}{" "}
-                </div>
-                <div>
-                  {oneAction.start_time
-                    ? `Start time: ${startTime}`
-                    : "Take as long as you want!"}{" "}
-                  {oneAction.end_time ? `End time: ${endTime}` : ""}{" "}
-                </div>
-              </div>
-              {/* // {oneAction.online && `Follow this link: ${oneAction.online_link} `} :{" "}
-				// {oneAction.in_person && `${oneAction.longitude} ${oneAction.latitude}`} */}
-            </Tab>
+	// }
 
-            <Tab
-              eventKey="requirements"
-              title="Requirements"
-              className="actionTabContent-css"
-            >
-              {oneAction.Requirements && oneAction.Requirements.length > 0 && (
-                <div className="container">
-                  {oneAction.Requirements.map((requirement) => (
-                    <div key={requirement.id}>
-                      <div className="row">
-                        <div className="col-6">{requirement.description}</div>
+	return (
+		<div>
+			<NavBar />
 
-                        <div className="col-3">
-                          {requirement.Volunteerships.length !=
-                            requirement.capacity &&
-                            `${requirement.Volunteerships.length} / ${requirement.capacity}`}
-                        </div>
-                        <div className="col-3">
-                          {requirement.Volunteerships &&
-                            requirement.Volunteerships.length > 0 && (
-                              <div>
-                                Volunteers:{" "}
-                                {requirement.Volunteerships.map(
-                                  (volunteership) => (
-                                    <div key={volunteership.id}>
-                                      <div>{volunteership.User.username}</div>
-                                    </div>
-                                  )
-                                )}
-                              </div>
-                            )}
-                        </div>
-                      </div>
-                      {/* <div className="joinButton">
-												<button
-													type="button"
-													className="btn btn-primary btn-sm">
-													Join
-												</button>
-											</div> */}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </Tab>
-          </Tabs>
-        </div>
-        <div className="countdown">
-          {days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
-            <div>Countdown is over!</div>
-          ) : (
-            <div>
-              Time until action!
-              <br></br>
-              {days} days, {hours} hours, {minutes} minutes, {seconds} seconds
-            </div>
-          )}
-        </div>
-        {/* back to actionsmenu page */}
-        <div className="buttonSection" id="singleButton">
-          <button className="backButton" onClick={() => navigate(-1)}>
-            Back
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="actioncontainer-css">
+				<h1> {oneAction.name} </h1>
+				<div className="tabs">
+					<Tabs defaultActiveKey="description" id="tabs">
+						<Tab
+							eventKey="description"
+							title="Description"
+							className="actionTabContent-css ">
+							<p>{oneAction.description}</p>
+							{oneAction.Keywords && (
+								<div className="keywordBadges">
+									<ul>
+										{oneAction.Keywords.map((keyword) => (
+											<li className="badge bg-primary" key={keyword.id}>
+												{keyword.keyword}
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+						</Tab>
+
+						<Tab
+							eventKey="location"
+							title="Location"
+							className="actionTabContent-css ">
+							<div>
+								<div className="container">
+									{oneAction.online
+										? `Follow this link: ${oneAction.online_link} `
+										: `Location: ${oneAction.longitude} ${oneAction.latitude}`}{" "}
+								</div>
+								<div>
+									{oneAction.start_time
+										? `Start time: ${startTime}`
+										: "Take as long as you want!"}{" "}
+									{oneAction.end_time ? `End time: ${endTime}` : ""}{" "}
+								</div>
+							</div>{" "}
+						</Tab>
+
+						<Tab
+							eventKey="requirements"
+							title="Requirements"
+							className="actionTabContent-css">
+							{oneAction.Requirements && oneAction.Requirements.length > 0 && (
+								<div className="container">
+									{oneAction.Requirements.map((requirement) => (
+										<div key={requirement.id}>
+											<div className="row">
+												<div className="col-6">{requirement.description}</div>
+												<div className="col-2">
+													<input
+														type="checkbox"
+														//checked={requirement.checked}
+														// onChange={() => handleCheckboxChange(requirement.id)}
+													/>
+												</div>
+												<div className="col-2">
+													{requirement.Volunteerships.length !==
+														requirement.capacity &&
+														`${requirement.Volunteerships.length} / ${requirement.capacity}`}
+												</div>
+
+												<div className="col-2">
+													{requirement.Volunteerships &&
+														requirement.Volunteerships.length > 0 && (
+															<div>
+																Volunteers:{" "}
+																{requirement.Volunteerships.map(
+																	(volunteership) => (
+																		<div key={volunteership.id}>
+																			<div>{volunteership.User.username}</div>
+																		</div>
+																	)
+																)}
+															</div>
+														)}
+												</div>
+											</div>
+										</div>
+									))}
+								</div>
+							)}
+						</Tab>
+					</Tabs>
+				</div>
+				<div className="container">
+					<div className="row">
+						<div className="col-6">
+							<div className="countdown">
+								{days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
+									<div>Countdown is over!</div>
+								) : (
+									<div>
+										Time until action!
+										<br></br>
+										{days} days, {hours} hours, {minutes} minutes, {seconds}{" "}
+										seconds
+									</div>
+								)}
+							</div>
+						</div>
+
+						<div className="col-3">
+							<div className="joinButton">
+								<button
+									type="button"
+									className="btn btn-primary btn-sm"
+									//onClick--> add user and req selected to the action
+								>
+									Join
+								</button>
+							</div>
+						</div>
+
+						{/* back to actionsmenu page */}
+						<div className="col-3">
+							<div className="buttonSection" id="singleButton">
+								<button className="backButton" onClick={() => navigate(-1)}>
+									Back
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
