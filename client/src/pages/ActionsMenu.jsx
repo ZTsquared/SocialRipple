b576a4c32950363cf78f91f8d6482e012c766345
Judit Calero <judit.calero84@gmail.com>
2023-12-05 13:31:02 +0100

    refactored and reestructured MainManu, improved styling, added Modal, started changing routes to show actions in modal

--- client/src/pages/ActionsMenu.jsx
+++ client/src/pages/ActionsMenu.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams, Outlet } from "react-router-dom";
 import ActionCard from "../components/ActionCard";
 import NavBar from "../components/NavBar";
 import FootBar from "../components/FootBar";
 import Map from "../components/Map";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 export default function ActionsMenu() {
   const navigate = useNavigate();
-  const [actions, setActions] = useState([]); // an array with ALL the actions                                             // lat & lng the map takes as its center
-  const [recommendedActions, setRecommendedActions] = useState([]); // the 3 recommended actions on top
+  const [actions, setActions] = useState([]);                               // an array with ALL the actions                                 
+  const [recommendedActions, setRecommendedActions] = useState([]);         // the 3 recommended actions on top
   const [displayActions, setDisplayActions] = useState({
     allVisible: false,
     group: false,
   });
+  
+  const {typeOfActions, ActionId} = useParams();
 
   useEffect(() => {
+    console.log(typeOfActions)
     getActions();
   }, []);
 
@@ -46,6 +52,10 @@ export default function ActionsMenu() {
         ...displayActions,
         allVisible: !displayActions.allVisible,
       });
+
+    if(event.target.name === "individual"){
+      typeOfActions = "individual";
+    }  
   }
 
   // I refactored the Group and individual pages into the mainmenu because right now ActionsIndividual.jsx and ActionsGroup.jsx are
@@ -60,89 +70,89 @@ export default function ActionsMenu() {
 
   // but if we prefer the other approach we can revert to a previous commit ^^
 
+  const [show, setShow] = useState(false);
+
+  const handleCloseModal = () => setShow(false);
+  const handleShowModal = (e) => {
+    
+    console.log(e.target)
+    setShow(true);
+  }
+
   return (
-    <div>
-      <NavBar />
+    <div className="container">
 
-      {displayActions.allVisible && (
-        <div>
-          {/* <Link to="/MainMenu/Individual" className="btn btn-success">
-          Individual actions
-        </Link> */}
+      <br /><br /><br />
+      <div className="row">
+        <div className="col-sm">
+          <h3>
+            {!typeOfActions ? "Recommended Actions" :
+            typeOfActions === "Group" ? "Group Actions" : "Individual Actions"}
+          </h3>
+          { typeOfActions ?
+          
+          actions
+            .filter((a) => (typeOfActions === "Group" ? a.is_group : !a.is_group))
+            .map((action, index) => (
+              <div onClick={handleShowModal} key={index} className="col-sm">
 
-          {/* <Link to="/MainMenu/Group" className="btn btn-success">
-          Group actions
-        </Link> */}
-          <button
-            name="group"
-            className="btn btn-success"
-            onClick={actionsToDisplayToggle}
-          >
-            Group Actions
-          </button>
-          <button
-            name="individual"
-            className="btn btn-success"
-            onClick={actionsToDisplayToggle}
-          >
-            Individual Actions
-          </button>
-        </div>
-      )}
-
-      <div className="container">
-        {!displayActions.allVisible && (
-          <div className="row">
-            <h3>Recommended for you</h3>
-            {recommendedActions.map((action, index) => (
-              <div key={index} className="col-sm">
-                <ActionCard action={action} />
-              </div>
-            ))}
-          </div>
-        )}
-
-        {displayActions.allVisible && (
-          <div className="row">
-            <h3>
-              {displayActions.group ? "Group Actions" : "Individual Actions"}
-            </h3>
-            {actions
-              .filter((a) => (displayActions.group ? a.is_group : !a.is_group))
-              .map((action, index) => (
-                <div key={index} className="col-sm">
+                <Link to={`/Actions/View/1`}>
                   <ActionCard action={action} />
-                </div>
-              ))}
-          </div>
-        )}
-
-        <div className="row">
-          <button
-            name="display_actions_switch"
-            onClick={actionsToDisplayToggle}
-            className="btn btn-success"
-          >
-            {displayActions.allVisible
-              ? "back to recomended"
-              : "Check out all upcomming actions"}
-          </button>
+                </Link>
+                
+              </div>
+            ))
+            :
+            recommendedActions
+            .map((action, index) => (
+              <div onClick={handleShowModal} key={index} className="col-sm">
+                <Link to={`/Actions/View/1`}>
+                  <ActionCard action={action} />
+                </Link>
+              </div>
+            ))
+            
+            }
         </div>
 
-        <div className="row">
-          <div className="col-sm">
-            <div>
-              <Map />
-            </div>
+      <div className="col-sm">
+
+          <div className="map_container">
+            <Map />
           </div>
-        </div>
 
-        <br />
-        <br />
-        <br />
-
-        <FootBar />
       </div>
+      </div>
+
+      <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+      <>
+      <Button variant="primary" onClick={handleShowModal}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Outlet></Outlet>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
     </div>
+      <br /><br /><br />
+    </div>
+
   );
 }
