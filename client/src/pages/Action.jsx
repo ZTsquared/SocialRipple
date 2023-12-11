430e640dc259da8768ffc63c8a905807b075900c
ClaireHowley <clairehowley2@gmail.com>
2023-12-11 11:29:31 +0100

    action page modified, modal function added

--- client/src/pages/Action.jsx
+++ client/src/pages/Action.jsx
@@ -5,11 +5,13 @@ import { useCountdown } from "../hooks/useCountdown";
 import { Tabs, Tab } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/NavBar";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export default function Action() {
 	const [oneAction, setOneAction] = useState({});
 	const { ActionId } = useParams();
-	const [requirements, setRequirements] = useOutletContext();
+	const [requirements, setRequirements] = useState([]);
 
 	let navigate = useNavigate();
 
@@ -76,7 +78,7 @@ export default function Action() {
 
 	function handleCheckboxChange(e) {
 		console.log(e);
-		e.preventDefault();
+
 		if (e.target.checked) setRequirements((r) => [...r, e.target.value]);
 		else setRequirements((r) => r.filter((req) => req !== e.target.value));
 	}
@@ -86,91 +88,117 @@ export default function Action() {
 		addVolunteerships();
 	};
 
+	function closeModal() {
+		setShow(false);
+		setTimeout(() => navigate("/Actions"), 300);
+	}
+
+	const [show, setShow] = useState(true);
+
 	return (
 		<div>
-			<div className="actioncontainer-css">
-				<div className="tabs">
-					<Tabs defaultActiveKey="description" id="tabs">
-						<Tab
-							eventKey="description"
-							title="Description"
-							className="actionTabContent-css ">
-							<p>{oneAction.description}</p>
-							<br></br>
-							{oneAction.Keywords && (
-								<div className="keywordBadges">
-									<ul>
-										{oneAction.Keywords.map((keyword) => (
-											<li className="badge bg-primary" key={keyword.id}>
-												{keyword.keyword}
-											</li>
-										))}
-									</ul>
-								</div>
-							)}
-						</Tab>
-
-						<Tab
-							eventKey="location"
-							title="Location"
-							className="actionTabContent-css ">
-							<div>
-								<div className="container">
-									{!oneAction.in_person && !oneAction.online && "Anywhere!"}
-									{oneAction.online && !oneAction.in_person && (
-										<div>Follow this link: {oneAction.online_link}</div>
-									)}
-									{oneAction.in_person && !oneAction.online && (
-										<div>Location: {oneAction.city}</div>
-									)}
-									{oneAction.in_person && oneAction.online && (
-										<>
-											<div>Location: {oneAction.city}</div>
-											<div>Follow this link: {oneAction.online_link}</div>
-										</>
-									)}
-									<div>
-										{oneAction.start_time
-											? `Start time: ${startTime}`
-											: "Take as long as you want!"}{" "}
+			<Modal
+				show={show}
+				onHide={closeModal}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered>
+				<Modal.Header closeButton>
+					<Modal.Title>{oneAction.name}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div>
+						<div className="actioncontainer-css">
+							<div className="tabs">
+								<Tabs defaultActiveKey="description" id="tabs">
+									<Tab
+										eventKey="description"
+										title="Description"
+										className="actionTabContent-css ">
+										<p>{oneAction.description}</p>
 										<br></br>
-										{oneAction.end_time ? `End time: ${endTime}` : ""}{" "}
-									</div>{" "}
-								</div>{" "}
-							</div>
-						</Tab>
+										{oneAction.Keywords && (
+											<div className="keywordBadges">
+												<ul>
+													{oneAction.Keywords.map((keyword) => (
+														<li className="badge bg-primary" key={keyword.id}>
+															{keyword.keyword}
+														</li>
+													))}
+												</ul>
+											</div>
+										)}
+									</Tab>
 
-						<Tab
-							eventKey="requirements"
-							title="Requirements"
-							className="actionTabContent-css">
-							<h4>What do you want to do?</h4>
-							<br></br>
-							{oneAction.Requirements && oneAction.Requirements.length > 0 && (
-								<div className="container">
-									{oneAction.Requirements.map((requirement) => (
-										<div key={requirement.id}>
-											<div className="row">
-												<div className="col-6">{requirement.description}</div>
-												<div className="col-2">
-													<input
-														value={requirement.id}
-														type="checkbox"
-														checked={
-															requirements.includes(requirement.id)
-																? "checked"
-																: null
-														}
-														onChange={handleCheckboxChange}
-													/>
-												</div>
-												{/*showing the capacity*/}
-												<div className="col-2">
-													{requirement.Users.length !== requirement.capacity &&
-														`${requirement.Users.length} / ${requirement.capacity}`}
-												</div>
-												<div className="col-2">
-													{/*
+									<Tab
+										eventKey="location"
+										title="Location"
+										className="actionTabContent-css ">
+										<div>
+											<div className="container">
+												{!oneAction.in_person &&
+													!oneAction.online &&
+													"Anywhere!"}
+												{oneAction.online && !oneAction.in_person && (
+													<div>Follow this link: {oneAction.online_link}</div>
+												)}
+												{oneAction.in_person && !oneAction.online && (
+													<div>Location: {oneAction.city}</div>
+												)}
+												{oneAction.in_person && oneAction.online && (
+													<>
+														<div>Location: {oneAction.city}</div>
+														<div>Follow this link: {oneAction.online_link}</div>
+													</>
+												)}
+												<div>
+													{oneAction.start_time
+														? `Start time: ${startTime}`
+														: "Take as long as you want!"}{" "}
+													<br></br>
+													{oneAction.end_time
+														? `End time: ${endTime}`
+														: ""}{" "}
+												</div>{" "}
+											</div>{" "}
+										</div>
+									</Tab>
+
+									<Tab
+										eventKey="requirements"
+										title="Requirements"
+										className="actionTabContent-css">
+										<h4>What do you want to do?</h4>
+										<br></br>
+										{oneAction.Requirements &&
+											oneAction.Requirements.length > 0 && (
+												<div className="container">
+													{oneAction.Requirements.map((requirement) => (
+														<div key={requirement.id}>
+															<div className="row">
+																<div className="col-6">
+																	{requirement.description}
+																</div>
+																<div className="col-2">
+																	<input
+																		value={requirement.id}
+																		type="checkbox"
+																		checked={
+																			requirements.includes(requirement.id)
+																				? "checked"
+																				: null
+																		}
+																		onChange={handleCheckboxChange}
+																	/>
+																</div>
+																{/*showing the capacity*/}
+																<div className="col-2">
+																	{requirement.Users.length !==
+																		requirement.capacity &&
+																		`${requirement.Users.length} / ${requirement.capacity}`}
+																</div>
+																<div className="col-2">
+																	{/*
 													{requirement.Volunteerships &&
 														requirement.Volunteerships.length > 0 && (
 															<div>
@@ -184,37 +212,42 @@ export default function Action() {
 																)}
 															</div>
 																	)}*/}
+																</div>
+															</div>
+														</div>
+													))}
 												</div>
-											</div>
-										</div>
-									))}
-								</div>
-							)}
-						</Tab>
-					</Tabs>
-				</div>
-			</div>
-			<div className="joinButton">
-				<button
-					type="button"
-					className="btn btn-primary btn-sm"
-					onClick={handleClick}>
-					Join
-				</button>
-			</div>
-			{!Number.isNaN(days) && (
-				<div className="countdown">
-					{days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
-						<div>Countdown is over!</div>
-					) : (
-						<div>
-							Time until action!
-							<br></br>
-							{days} days, {hours} hours, {minutes} minutes, {seconds} seconds
+											)}
+									</Tab>
+								</Tabs>
+							</div>
 						</div>
-					)}
-				</div>
-			)}
+
+						{!Number.isNaN(days) && (
+							<div className="countdown">
+								{days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 ? (
+									<div>Countdown is over!</div>
+								) : (
+									<div>
+										Time until action!
+										<br></br>
+										{days} days, {hours} hours, {minutes} minutes, {seconds}{" "}
+										seconds
+									</div>
+								)}
+							</div>
+						)}
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={closeModal}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClick}>
+						Join!
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	);
 }
